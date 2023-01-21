3d72801a9eb8bea17331a27c5cb2d3ff2fe95e22
Zaccco <samuelcollskog98@gmail.com>
2023-01-21 23:03:54 +0000

    Add revealCard and checkIfMatch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,52 @@
 document.addEventListener("DOMContentLoaded", function() {
-    for (card in cards) {
+    
+    cards = document.getElementsByClassName("card");
+
+    for (let card of cards) {
         card.addEventListener("click", revealCard);
     }
 })
 
+let alreadyFlipped = false;
+let first;
+let second;
 
+function revealCard() {
+    this.classList.add('reveal');
 
+    if (!alreadyFlipped) {
+        // This means it's the first card flipped
+        alreadyFlipped = true;
+        first = this;
+    } else {
+        // This means it's the second card
+        alreadyFlipped = false;
+        second = this;
 
+        checkIfMatch();
+    }
+}
+
+function checkIfMatch() {
+    if (first.dataset.name === second.dataset.name) {
+        // if it matches
+        removeMatchingPair();
+    } else {
+        // not a match
+        first.classList.remove("reveal");
+        second.classList.remove("reveal");
+    }
+}
+
+function removeMatchingPair() {
+    
+}
+
+function randomizeOrder() {
+
+}
+
+function restartGame() {
+    
+}
 
